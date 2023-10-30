becfbff42c48098e8ba6dab3adc073ab17af9b94
Taweechai Yuenyang <krumii.it@gmail.com>
2023-10-30 11:14:51 +0700

    update theme

--- webbase/settings.py
+++ webbase/settings.py
@@ -378,7 +378,9 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar_nav_compact_style": True,
     "sidebar_nav_legacy_style": False,
     "sidebar_nav_flat_style": True,
-    "theme": "default",
+    # "theme": "default",
+    # "theme": "journal",
+    'theme': 'united',
     "dark_mode_theme": None,
     "button_classes": {
         "primary": "btn-primary",
